dfa56c5e0cd2b489911fedf2811cdda42df03d51
Atharv Parlikar <atharvparlikar@gmail.com>
2023-03-26 13:26:35 +0530

    some changes

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,12 @@ use std::fs::File;
 use std::io::{Read, Write};
 use std::path::Path;
 
-fn encoder(bitmap: &Vec<[[u8; 3]; 1080]>, file_path: String) -> Result<(), std::io::Error> {
+fn encoder(bitmap: &Vec<Vec<[u8; 3]>>, file_path: String) -> Result<(), std::io::Error> {
     let mut img = RgbImage::new(1920, 1080);
 
     for x in 0..bitmap.len() {
         for y in 0..bitmap[x].len() {
-            img.put_pixel(x as u32, y as u32, Rgb(bitmap[x][y]));
+            img.put_pixel(y as u32, x as u32, Rgb(bitmap[x][y]));
         }
     }
 
@@ -26,83 +26,132 @@ fn encoder(bitmap: &Vec<[[u8; 3]; 1080]>, file_path: String) -> Result<(), std::
 fn decoder(img_path: &str) -> Result<Vec<u8>, std::io::Error> {
     let mut data: Vec<u8> = Vec::new();
     let img = ImageReader::open(img_path)?.decode().unwrap().to_rgb8();
-    for x in 0..1920 {
-        for y in (0..1080).step_by(3) {
-           for d in img.get_pixel(x,y).0 {
-                data.push(d);
-            }
+    for i in img.pixels() {
+        for d in i.0 {
+            data.push(d);
         }
-    } 
+    }
     return Ok(data);
 }
 
-fn create_bitmap(file_path: &str) -> Vec<[[u8; 3]; 1080]> {
-    let mut bitmap = vec![[[0 as u8; 3]; 1080]; 1920];
-    let path = Path::new(file_path);
+fn split_num(num: u16) -> (u8, u8) {
+    let a = (num >> 8) as u8;
+    let b = num as u8;
+    return (a, b);
+}
 
-    let mut file = match File::open(&file_path) {
-        Ok(file) => file,
-        Err(why) => panic!("couldn't open '{}': {}", path.display(), why),
-    };
+fn join_num(nums: (u8, u8)) -> u16 {
+    let a = (nums.0 as u16) << 8;
+    return a | (nums.1 as u16);
+}
 
-    let mut data: Vec<u8> = Vec::new();
-    match file.read_to_end(&mut data) {
-        Ok(_) => (),
-        Err(err) => panic!("error occured: {}", err),
+fn bitmap_(data: Vec<u8>) -> Result<Vec<Vec<[u8; 3]>>, std::io::Error> {
+    if data.len() > (1920 * 1080 * 3) {
+        panic!("The limit of bitmap function [6220794 bytes] exceeded!");
     }
 
-    let mut new_data: Vec<[u8; 3]> = Vec::new();
+    let mut data_:Vec<[u8; 3]> = Vec::new();
+    let len = data.len();
 
-    let mut pointer = 2;
-
-    while pointer <= data.len() - 1 {
-        new_data.push([data[pointer - 2], data[pointer - 1], data[pointer]]);
-        pointer += 3;
+    for i in (2..(len - (len % 3))).step_by(3) {
+        data_.push([data[i-2], data[i-1], data[i]]);
     }
 
-    if data.len() % 3 == 2 {
-        let ln = data.len();
-        new_data.push([data[ln - 2], data[ln - 1], 0]);
-    } else if data.len() % 3 == 1 {
-        new_data.push([data[data.len() - 1], 0, 0]);
+    let extra = data.len() % 3;
+
+    match extra {
+        1 => data_.push([data[len - 1], 0, 0]),
+        2 => data_.push([data[len - 2], data[len - 1], 0]),
+        _ => {}
     }
 
-    let mut x = 0;
-    let mut y = 0;
-    for pixel in new_data {
-        for i in x..x + 3 {
-            for j in y..y + 3 {
-                bitmap[i][j] = pixel;
+    let mut bitmap: Vec<Vec<[u8;3]>> = Vec::new();
+
+    if data_.len() > (1920 * 1079) - 1 {
+        println!("if block");
+        let mut counter = 0;
+        for _ in 0..(data_.len() / 3) {
+            bitmap.push(data_[counter..(counter + 1920)].to_vec());
+            counter += 1920;
+        }
+        bitmap.push(data_[counter..data_.len()].to_vec());
+        for _ in 0..(data_.len() - counter - 2) {
+            bitmap[1080 - 1].push([0 as u8; 3]);
+        }
+    }
+    
+    else {
+        println!("else block");
+        let mut counter = 0;
+        for _ in 0..(data_.len() / 1920) {
+            bitmap.push(data_[counter..(counter + 1920)].to_vec());
+            counter += 1920;
+        }
+        println!("necessary pixels painted");
+        bitmap.push(data_[counter..data_.len()].to_vec());
+        for _ in 0..(1080 - (counter / 1920) - 1) {
+            bitmap.push(vec![[0 as u8; 3]; 1920]);
+        }
+        println!("empty pixels painted");
+        let last_row_num = split_num(( counter / 1920) as u16);
+        let last_pixel_num = split_num((data_.len() % 1920) as u16);
+        bitmap[1080 - 1][1920 - 2] = [0, last_row_num.0, last_row_num.1];
+        bitmap[1080 - 1][1920 - 1] = [0, last_pixel_num.0, last_pixel_num.1];
+    } 
+
+    println!("bitmap function is safe");
+    return Ok(bitmap);
+}
+
+fn bitmap(data: Vec<u8>) -> Vec<Vec<[u8; 3]>> {
+    let mut pixels: Vec<[u8; 3]> = data.chunks(3)
+        .map(|chunk| {
+            let mut pixel = [0; 3];
+            for (i, val) in chunk.iter().enumerate() {
+                pixel[i] = *val;
             }
+            pixel
+        })
+        .collect();
+
+    let missing_pixels = 1920 * 1080 - pixels.len();
+    pixels.extend(std::iter::repeat([0; 3]).take(missing_pixels));
+
+    let mut rows: Vec<Vec<[u8; 3]>> = Vec::with_capacity(1080);
+    for _ in 0..1079 {
+        let mut row: Vec<[u8; 3]> = Vec::with_capacity(1920);
+        for _ in 0..1920 {
+            let pixel = pixels.pop().unwrap();
+            row.push(pixel);
         }
-        if y >= 1069 {
-            x += 3;
-            y = 0;
-        }
-        y += 3;
+        rows.push(row);
     }
 
-    return bitmap;
+    // Last row with 2 empty pixels
+    let mut last_row: Vec<[u8; 3]> = Vec::with_capacity(1920);
+    for _ in 0..1918 {
+        let pixel = pixels.pop().unwrap();
+        last_row.push(pixel);
+    }
+    last_row.push([0; 3]);
+    last_row.push([0; 3]);
+    rows.push(last_row);
+
+    rows
 }
-
-fn bitmap(file_path: &str) -> Result<(), std::io::Error>{
-    let file = match File::open(Path::new(file_path)) {
-        Ok(file) => file,
-        Err(err) => panic!("{}", err)
-    };
-
-    return Ok(());
-}
-
 fn main() -> Result<(), std::io::Error> {
-    // let bitmap = create_bitmap("./test");
-    // encoder(&bitmap, "./encoded.png".to_string());
-    // let decoded = decoder("./encoded.png")?;
-    // File::create("./data")?.write(&decoded);
-    let mut img = RgbImage::new(1920, 1080);
-    for x in 0..1920 {
-        img.put_pixel(x, 0, Rgb([255, 0, 0]));
-    }
-    img.save_with_format("./encoded.png", ImageFormat::Png);
+    let mut file = File::open(Path::new("/home/atharv/dev/VidCoder/src/main.rs"))?;
+    let mut data:Vec<u8> = Vec::new();
+    file.read_to_end(&mut data);
+    println!("{}", data.len());
+    let bitmap_ = bitmap(data.clone())?;
+    println!("{} {}", bitmap_.len(), bitmap_[0].len());
+    encoder(&bitmap_, "encoded.png".to_string());
+
+    // let res = decoder("encoded.png")?;
+    // let mut decoded = File::create(Path::new("decoded"))?;
+    // decoded.write(&res);
+    println!("Done!");
+
     return Ok(());
 }
